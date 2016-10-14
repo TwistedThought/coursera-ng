ae72e91d978e21076897c9bc7f7caca1939403d3
Alexey_Mikhailov <alexxxgf@mail.ru>
2016-10-14 20:56:35 +0300

    app done

---
+++ mod1-sol/app.js
@@ -0,0 +1,44 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.list = '';
+  $scope.message = '';
+  $scope.font = '';
+  $scope.border = '';
+
+  $scope.count = function () {
+    var arr = $scope.list.split(',');
+    var cnt = 0;
+    for (var i = 0; i < arr.length; i++) {
+    	if (arr[i].trim() != '') {
+    		cnt++
+    	}
+    }
+    return cnt;
+  };
+
+  $scope.showMessage = function (count) {
+  	if ($scope.list == '') {
+  		$scope.message = 'Please enter data first';
+  		$scope.font = 'color: red'
+  		$scope.border = 'border: 1px solid red'
+  	}
+  	else if (count <= 3) {
+  		$scope.message = 'Enjoy!';
+  		$scope.font = 'color: green'
+  		$scope.border = 'border: 1px solid green'
+  	}
+  	else {
+  		$scope.message = 'Too much!';
+   		$scope.font = 'color: green'
+  		$scope.border = 'border: 1px solid green'
+  	};
+  };
+};
+
+})();
